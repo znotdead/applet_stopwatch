6b78f4bccae1703410e0c3b232e1aa7f226723c3
znotdead <zhirafchik@gmail.com>
2010-08-20 19:42:23 +0300

    for debug perposes

--- stopwatch_applet.py
+++ stopwatch_applet.py
@@ -6,6 +6,8 @@ import gtk
 import gnomeapplet
 import gobject
 
+import sys
+
 def background_show(applet):
     print "background: ", applet.get_background()
 
@@ -18,6 +20,17 @@ def sample_factory(applet, iid):
     gobject.timeout_add(1000, background_show, applet)
     return True
 
+
+if len(sys.argv) == 2 and sys.argv[1] == "run-in-window":
+        main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        main_window.set_title("Python Applet")
+        main_window.connect("destroy", gtk.mainquit)
+        app = gnomeapplet.Applet()
+        sample_factory(app, None)
+        app.reparent(main_window)
+        main_window.show_all()
+        gtk.main()
+        sys.exit()
 
 print "Starting factory"
 gnomeapplet.bonobo_factory("OAFIID:GNOME_PythonAppletStopwatch_Factory",
